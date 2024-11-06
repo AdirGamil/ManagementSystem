62bb70fdf4eeb6fe0fd2f1e44fb7574143b1c1ab
AdirGamil <2000adir2000@gmail.com>
2024-11-06 15:09:13 +0200

    Add routing for pages

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -3,18 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Footer } from './cmps/Footer'
 import { Navbar } from './cmps/Navbar.jsx'
+import { AdminPanel } from './pages/AdminPanel'
+import { Coupons } from './pages/Coupons'
+import { Login } from './pages/Login'
+import { Users } from './pages/Users'
 
 export function RootCmp() {
   return (
     <Router>
       <section className="main-container">
-        <Navbar/>
+        <Navbar />
         <main>
           <Routes>
             <Route path="" element={<Home />} />
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/coupons" element={<Coupons />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/users" element={<Users />} />
           </Routes>
         </main>
-        <Footer/>
+        <Footer />
       </section>
     </Router>
   )
