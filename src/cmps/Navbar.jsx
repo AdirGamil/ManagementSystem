6421e92740cb8804ce34c9e42f8d9952f96ab748
AdirGamil <2000adir2000@gmail.com>
2024-11-08 11:05:27 +0200

    Add mobile styling to Navbar

--- src/cmps/Navbar.jsx
+++ src/cmps/Navbar.jsx
@@ -1,45 +1,57 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
   const getActiveClass = ({ isActive }) => (isActive ? 'active' : '')
+  const toggleMenu = () => setIsOpen(!isOpen)
+
+  const closeMenu = () => setIsOpen(false)
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <h1>Logo</h1>
       </div>
-      <ul className="navbar-links">
+
+      <div className="navbar-menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <li>
-          <NavLink to="" className={getActiveClass}>
+          <NavLink to="" className={getActiveClass} onClick={closeMenu}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/admin" className={getActiveClass}>
+          <NavLink to="/admin" className={getActiveClass} onClick={closeMenu}>
             Admin Panel
           </NavLink>
         </li>
         <li>
-          <NavLink to="/coupons" className={getActiveClass}>
+          <NavLink to="/coupons" className={getActiveClass} onClick={closeMenu}>
             Coupons
           </NavLink>
         </li>
         <li>
-          <NavLink to="/users" className={getActiveClass}>
+          <NavLink to="/users" className={getActiveClass} onClick={closeMenu}>
             Users
           </NavLink>
         </li>
         <li>
-          <NavLink to="/reports" className={getActiveClass}>
+          <NavLink to="/reports" className={getActiveClass} onClick={closeMenu}>
             Reports
           </NavLink>
         </li>
+        <li className="navbar-login">
+          <NavLink to="/login" className={getActiveClass} onClick={closeMenu}>
+            Login
+          </NavLink>
+        </li>
       </ul>
-      <div className="navbar-login">
-        <NavLink to="/login" className={getActiveClass}>
-          Login
-        </NavLink>
-      </div>
     </nav>
   )
 }
