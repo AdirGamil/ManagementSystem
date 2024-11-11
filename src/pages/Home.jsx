c9ccb78ce11f9247e6ccf5f1deda0aa772e022d0
AdirGamil <2000adir2000@gmail.com>
2024-11-11 14:46:30 +0200

    Styling home page & add info about the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom'
+import adminImage from '../assets/img/admin-system.png'
+
 export function Home() {
-    return (
-        <div>
-           hello from Home
-        </div>
-    )
+  return (
+    <div className="home">
+      {/* Section 1: Overview */}
+      <section className="section overview">
+        <h1>Welcome to the Management System</h1>
+        <p>
+          This platform enables seamless management of users, coupons, and
+          reports. Designed for efficiency and ease of use, it's perfect for
+          admins seeking control and insights.
+        </p>
+        <Link to="#system-sections" className="start-button">
+          Start Now
+        </Link>
+      </section>
+    </div>
+  )
 }
