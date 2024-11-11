03bf5d87ca5e7869218b5ca61e743146e0d896b0
AdirGamil <2000adir2000@gmail.com>
2024-11-11 09:33:28 +0200

    Add more demo data

--- src/cmps/UserSystem/UserList.jsx
+++ src/cmps/UserSystem/UserList.jsx
@@ -4,15 +4,17 @@ export function UserList({ users }) {
   return (
     <div className="user-list">
       <h2>User Management</h2>
-      <button className="add-user-button">
-        Add User
-      </button>
+      <button className="add-user-button">Add User</button>
 
       <div className="users">
         {users.map((user) => (
           <UserPreview
             key={user.id}
+            id={user.id}
             username={user.username}
+            password={user.password}
+            registrationDate={user.registrationDate}
+            lastLoginDate={user.lastLoginDate}
             role={user.role}
           />
         ))}
--- src/cmps/UserSystem/UserPreview.jsx
+++ src/cmps/UserSystem/UserPreview.jsx
@@ -1,8 +1,19 @@
-export function UserPreview({ username, role }) {
+export function UserPreview({
+  id,
+  username,
+  role,
+  password,
+  registrationDate,
+  lastLoginDate,
+}) {
   return (
     <div className="user-preview">
       <div className="user-info">
+        <p>ID: {id}</p>
         <p>Username: {username}</p>
+        <p>Password: {password}</p>
+        <p>Registration Date: {registrationDate}</p>
+        <p>Last Login Date: {lastLoginDate}</p>
         <p>Role: {role}</p>
       </div>
       <button className="edit-button">Edit</button>
