f99bc4c7b0271f6752c9b988a574feb97ca7fac0
AdirGamil <2000adir2000@gmail.com>
2024-11-11 15:46:32 +0200

    Install react-typed & styling responsive home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,38 @@
 import { Link } from 'react-router-dom'
 import adminImage from '../assets/img/admin-system.png'
-
+import { ReactTyped } from 'react-typed'
 export function Home() {
   return (
     <div className="home">
       {/* Section 1: Overview */}
       <section className="section overview">
+      <div className="overview-content">
+        <h4 className="sub-heading">Your Complete Solution for Management</h4>
         <h1>Welcome to the Management System</h1>
+        <span>
+          <ReactTyped
+            strings={[
+              'User Management',
+              'Coupon Management',
+              'Reports Management',
+            ]}
+            typeSpeed={50}
+            backSpeed={30}
+            loop
+          />
+        </span>
         <p>
           This platform enables seamless management of users, coupons, and
           reports. Designed for efficiency and ease of use, it's perfect for
           admins seeking control and insights.
         </p>
         <Link to="#system-sections" className="start-button">
-          Start Now
+          Get Started
         </Link>
+        </div>
       </section>
+
+    
     </div>
   )
 }
